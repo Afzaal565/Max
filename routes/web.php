877da40daaf211ef2ef539b3d83f9fb5c868e182
Afzaal565 <afzaalhussain565@gmail.com>
2024-04-29 00:13:47 +0500

    remove show pages

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('companies', CompanyController::class);
-    Route::resource('employees', EmployeeController::class);
+    Route::resource('companies', CompanyController::class)->except('show');
+    Route::resource('employees', EmployeeController::class)->except('show');
+    
 });
 
 
